a36ade37f6072ff8de5e4b58066ee99d33d5faf3
KSanchezNazate <113851163+KSanchezNazate@users.noreply.github.com>
2023-03-25 21:33:24 +0100

    hecho

--- src/main/java/org/example/ClaseCadena.java
+++ src/main/java/org/example/ClaseCadena.java
@@ -1,2 +1,33 @@
-package org.example;public class ClaseCadena {
+package org.example;
+
+import java.util.Scanner;
+
+public class ClaseCadena {
+    public static String DevuelveIniciles (String nombreCompleto){
+        if(nombreCompleto == null || nombreCompleto.trim().equals("")){
+            return "";
+        }
+        //el \\s+ es para que tome los espacios en blanco
+        String[] partesNombre = nombreCompleto.split("\\s+");
+        StringBuilder iniciales = new StringBuilder();
+        for(String parte : partesNombre){
+            if(parte.length()>0){
+                iniciales.append(parte.charAt(0)).append(".");
+            }
+        }
+        return iniciales.toString();
+    }
+
+    public void menu(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa tu nombre y apellido");
+        String nombreCompleto = sc.nextLine();
+
+        String iniciales = DevuelveIniciles(nombreCompleto);
+        if(iniciales.isEmpty()){
+            System.out.println("Nombre invalido");
+        }else{
+            System.out.println("Tus iniales son: " + iniciales);
+        }
+    }
 }
